142b722b3a6bb65b981ba7c67f2838d785b25ce2
clach04 <clach04@gmail.com>
2024-01-06 10:50:20 -0800

    Added line_check.py

--- README.md
+++ README.md
@@ -4,7 +4,43 @@
 
 Is it safe!?
 
-Is this file US-ASCII (7 bit) safe?
+  * Is this file US-ASCII (7 bit) safe?
+  * Is this file UTF-8 safe?
+  * Is this file cp1252 safe?
+  * Is this file ... safe?
+
+
+## Tools
+
+Python 3.x and 2.7 (maybe 2.x?) tools:
+
+  * unicode_dentist.py - find invalid **bytes** in a single file, will report statistics and context
+  * wildcard_unicode_dentist.py - same as above but handles multiple files in a list (potentially with wild cards, even under Microsoft Windows)
+  * line_check.py - display lines that are not in the expected encoding, along with line number and final "bad" line number count
+
+## Examples
+
+Run on this readme.
+
+### line_check example
+
+    $ ./line_check.py us-ascii README.md
+    README.md:
+    28:'Euro symbol: \x80'
+    29:'Copyright symbol: \xa9'
+    30:'Euro symbol (again): \x80'
+    48:'    README.md:10: 128 \x80 @ 152'
+    50:'    Euro symbol: \x80'
+    52:'    README.md:11: 169 \xa9 @ 171'
+    54:'    Copyright symbol: \xa9'
+    56:'    README.md:12: 128 \x80 @ 193'
+    58:'    Euro symbol (again): \x80'
+    61:'    169 0xa9 not valid us-ascii character \xa9 occurrences 1'
+    62:'    128 0x80 not valid us-ascii character \x80 occurrences 2'
+    11 problem lines in README.md
+
+
+### unicode_dentist example
 
 Example usage:
 
@@ -47,3 +83,4 @@ Sample output non ASCII:
     169 0xa9 not valid us-ascii character © occurrences 1
     128 0x80 not valid us-ascii character € occurrences 2
     =================================================================
+
---
+++ line_check.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: ascii -*-
+# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
+#
+# Unicode Dentist, Is it safe? http://www.imdb.com/title/tt0074860/quotes
+# Copyright (C) 2011  Chris Clark
+#
+# Determine if file content is "safe", e.g. is it pure ASCII?
+# Can specifiy an encoding check but it always looks for non-ASCII bytes.
+# Encoding is used for display BUT it only really works for Single Byte encodings.
+#
+
+import glob
+import os
+import sys
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    '''Usage:
+        encoding filename(s)_to_check
+    '''
+    # nasty argv command line argument parsing
+    expect_encoding = 'us-ascii'
+    filename_pattern = '*'
+
+    guess_encoding = 'cp1252'
+    guess_encoding = 'utf-8'
+
+    if len(argv) > 1:
+        # Assume filename and optional encoding
+        # no need for web server mode (file mode it more reliable, we have the raw bytes)
+        expect_encoding = argv[1]
+
+        try:
+            filename_pattern = argv[2]
+        except IndexError:
+            filename_pattern = '*'
+
+    nl_bytes = '\n'.encode(expect_encoding)
+    ll_bytes = '\r'.encode(expect_encoding)
+
+    for filename in glob.glob(filename_pattern):
+        print('%s:' % filename)
+        f = open(filename, 'rb')
+        entire_file_bytes = f.read()
+        f.close()
+        problem_lines = 0
+        for line_count, line_bytes in enumerate(entire_file_bytes.split(nl_bytes), 1):
+            #print(line_count)
+            try:
+                line_bytes.encode(expect_encoding)
+            except UnicodeDecodeError:
+                print('%d:%r' % (line_count, line_bytes))
+                problem_lines += 1
+        print('%d problem lines in %s' % (problem_lines, filename))
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
